d5cec7f13019a6a93e81e3c71dd1fc9ed3366507
unknown <shettyshailendra3@gmail.com>
2025-07-13 13:59:44 +0530

    the update , delete and insert user is done

--- src/app.js
+++ src/app.js
@@ -5,14 +5,64 @@ const User = require("./models/user")
 
 const app = express();
 
+app.use(express.json())
+
+app.get("/user", async (req, res)=>{
+    const userEmail = req.body.emailId
+
+    try{
+        const users = await User.find({emailId : userEmail})
+        if(users.length === 0){
+            res.status(404).send("user not found")
+        } else{
+            res.send(users)
+        }
+    } catch(err){
+        res.status(400).send("something went wrong!")
+    }
+    
+})
+
+app.delete("/user", async(req, res)=>{
+    try{
+        // const user = await User.findByIdAndDelete({_id: req.body.id})
+        const user = await User.findByIdAndDelete(req.body.id)
+        if(!user){
+            res.status(404).send("user not found")
+        } else{
+            res.send(user)
+        }
+    } catch(err){
+        res.status(400).send("something went wrong!")
+    }
+    
+})
+
+app.patch("/user", async(req, res)=>{
+    try{
+        const updateData = req.body
+        const user = await User.findOneAndUpdate({_id : req.body.id}, updateData)
+        res.send(user)
+    } catch (err){
+        res.status(400).send("something went wrong!")
+    }
+    
+})
+
+app.get("/feed", async (req, res)=>{
+    try{
+        const users = await User.find({})
+        res.send(users)
+    } catch(err){
+        res.status(400).send("something went wrong!")
+    }
+    
+})
+
+
 
 app.post("/signup", async (req, res)=>{
-    const user = new User({
-        firstName : "this",
-        lastName : "this",
-        emailId : "this@gmail.com",
-        password : "this"
-    })
+    const user = new User(req.body)
 
     try{
         await user.save()
