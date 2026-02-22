595b98b85d6fde1a86bca3a65d54b86f9b2a38b8
unknown <shettyshailendra3@gmail.com>
2026-02-22 14:40:16 +0530

    profile updation api created

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,19 +1,31 @@
 const validator = require('validator')
 
-const validateSignUpData = (req)=>{
-    const {firstName, lastName, emailId, password}= req.body
+const validateSignUpData = (req) => {
+    const { firstName, lastName, emailId, password } = req.body
 
-    if(!firstName || !lastName){
+    if (!firstName || !lastName) {
         throw new Error("Please enter valid name...")
     }
-    else if(!validator.isEmail(emailId)){
+    else if (!validator.isEmail(emailId)) {
         throw new Error("Enter a valid emailId...")
     }
-    else if(!validator.isStrongPassword(password)){
+    else if (!validator.isStrongPassword(password)) {
         throw new Error("Enter a strong password...")
     }
 }
 
+
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName", "lastName", "emailId", "photoUrl", "gender", "age", "about", "skills"];
+
+    const isEditAllowed = Object.keys(req.body).every((field) => 
+        allowedEditFields.includes(field)
+    );
+
+    return isEditAllowed;
+}
+
 module.exports = {
-    validateSignUpData
+    validateSignUpData,
+    validateEditProfileData
 }
